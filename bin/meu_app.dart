a07b4f26ea2d8c656144849787fbcc24320ac448
Vaalls <bielvalls4@gmail.com>
2024-10-08 20:21:43 -0300

    Capturando e manipulando entrada de dados fornecido pelo user.

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
 void main() {
-  print("Hello Word!"); 
+  print("Olá me chamo GABRIEL. Qual seu nome?");
+   // Função capturar entrada
+  var entrada =  stdin.readLineSync();
+  print("Muito prazer, $entrada. Vamos programar!!");
 }
