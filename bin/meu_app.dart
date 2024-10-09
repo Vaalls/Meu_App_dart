aa7b976e034f9408e6929972930291097e9f33ba
Vaalls <bielvalls4@gmail.com>
2024-10-09 11:57:38 -0300

    Criando funções de calculos para nossa calculadora.

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -6,6 +6,25 @@ void main() {
   double numeroUm = double.parse(stdin.readLineSync()!);
   double numeroDois = double.parse(stdin.readLineSync()!);
 
-  // ! = Garantimos que os valores nao viram nulos
-  print(numeroUm + numeroDois);
+  // Criando função de soma
+  void soma(){
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao(){
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao(){
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao(){
+    print(numeroUm * numeroDois);
+  }
+
+  soma();
+  subtracao();
+  divisao();
+  multiplicacao();
 }
