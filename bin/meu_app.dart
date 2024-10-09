f075b4b49d5b42536aed6900202a07c351705d2d
Vaalls <bielvalls4@gmail.com>
2024-10-09 16:06:47 -0300

    Tratando erros de cód, referente a entrada de dados.

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 // Null safety = Segurança contra valores nulos
 
 void main() {
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  double numeroDois = double.parse(stdin.readLineSync()!);
-  String operacao = stdin.readLineSync()!;
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
 
   void soma(){
     print(numeroUm + numeroDois);
@@ -21,6 +21,26 @@ void main() {
 
   void multiplicacao(){
     print(numeroUm * numeroDois);
+  }
+
+  String? entrada = stdin.readLineSync();
+
+  if (entrada != null){
+    if(entrada != ""){
+      numeroUm = double.parse(entrada);
+    }
+  }
+
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    if (entrada != ""){
+      numeroDois = double.parse(entrada);
+    }
+  }
+
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    operacao = entrada;
   }
 
   switch (operacao){
